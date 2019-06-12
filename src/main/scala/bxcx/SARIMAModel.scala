9ca5cd9419ad724de43837f258e4a8783fa0d054
kkashkovskij <kkashkovskij@naumen.ru>
2019-06-12 16:15:38 +0300

    init

--- src/main/scala/bxcx/SARIMAModel.scala
+++ src/main/scala/bxcx/SARIMAModel.scala
@@ -2,14 +2,19 @@ package bxcx
 
 import breeze.linalg.{DenseMatrix, DenseVector, pinv}
 
-class SARIMAModel(data: Vector[Double], p: Int, d: Int, q: Int) {
+case class Season(period: Int, p: Int, d: Int, q: Int)
 
-    def finiteDiffs(vector: Vector[Double], order: Int): Vector[Double] = {
+class SARIMAModel(data: Vector[Double], p: Int, d: Int, q: Int, seasons: Seq[Season] = Nil) {
+
+    val diffs: Vector[Double] = finiteDiffs(data, d)
+    val boxcoxed: Vector[Double] = BoxCox.transform(data.toList).toVector
+
+    def finiteDiffs(vector: Vector[Double], order: Int, lag: Int = 0): Vector[Double] = {
         if (order < 1) vector
         else {
-            if (vector.size < order + 1) throw new RuntimeException()
-            else finiteDiffs(vector.indices.dropRight(1)
-                .map(idx => vector(idx + 1) - vector(idx)).toVector, order - 1)
+            if (vector.size < order + 1 + lag) throw new RuntimeException()
+            else finiteDiffs(vector.indices.dropRight(1 + lag)
+                .map(idx => vector(idx + 1 + lag) - vector(idx)).toVector, order - 1, lag)
         }
     }
 
@@ -23,15 +28,26 @@ class SARIMAModel(data: Vector[Double], p: Int, d: Int, q: Int) {
     def solve(a: DenseMatrix[Double], b: DenseVector[Double]): DenseVector[Double] =
         pinv(a) * b
 
-    val diffs: Vector[Double] = finiteDiffs(data, d)
-    val boxcoxed: Vector[Double] = BoxCox.transform(data.toList).toVector
-
-    val coefVectors = data.indices.drop(Seq(p, q).max).dropRight(d).map {
-        idx => DenseVector(((1.0 +: (1 to p).map(i => diffs(idx - i))) ++ (1 to q).map(j => boxcoxed(idx - j))).toArray)
+    def createVectors(dropFirst: Int, dropLast: Int) = {
+        data.indices.drop(dropFirst).dropRight(dropLast).map { idx =>
+            var v = (1.0 +: (1 to p).map(i => diffs(idx - i))) ++ (1 to q).map(j => boxcoxed(idx - j))
+            seasons.foreach { season =>
+                val sDiffs = finiteDiffs(data, season.d, season.period)
+                if (idx >= Seq(season.p, season.q, season.period).max) {
+                    v = v ++ (season.period until season.period + season.p).map(i => sDiffs(idx - i))
+                    v = v ++ (season.period until season.period + season.q).map(i => boxcoxed(idx - i - season.period))
+                } else v = v :+ 0 :+ 0
+            }
+            DenseVector(v: _ *)
+        }
     }
+
+    val coefVectors = createVectors(Seq(p, q).max, d)
+
     val resultV = data.indices.drop(Seq(p, q).max).dropRight(d).map { idx =>
         diffs(idx) - boxcoxed(idx)
     }
+
     val coefMatrix: DenseMatrix[Double] = DenseMatrix(coefVectors: _ *)
     val resultVector: DenseVector[Double] = DenseVector(resultV: _ *)
 
