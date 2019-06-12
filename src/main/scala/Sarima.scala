4135973007ea8c13eeda6982253b2142f9af5706
kkashkovskij <kkashkovskij@naumen.ru>
2019-06-12 17:36:50 +0300

    init

--- src/main/scala/Sarima.scala
+++ src/main/scala/Sarima.scala
@@ -1,20 +1,22 @@
-import breeze.linalg.{DenseMatrix, DenseVector, pinv}
-import bxcx.BoxCox
+import bxcx.{SARIMAModel, Season}
+
+import scala.io.Source
 
 object Sarima extends App {
+    def avg(vector: Vector[Double]) = {
+        vector.sum/vector.size
+    }
 
+    val data = Source.fromFile("/home/kkashkovskij/IdeaProjects/SARIMA/src/main/resources/mow_spb_ts_fill_zeros.csv").getLines.drop(1).map { line =>
+        line.split(",")(1).toDouble
+    }.toVector
+    val (data1, data2) = data.splitAt(730)
 
+    val model = new SARIMAModel(data1, 1, 1, 1, Seq(Season(7, 1, 1, 1)))
+    val result = model.predict(data2.size)
 
+    val diff = data2.zip(result.drop(730)).map{ case (b, p) => b - p}
 
-    val ls = Vector[Double](1, 3, 3, 4, 5)
+    println(avg(diff))
 
-    println(finiteDiffs(ls, 1))
-    println(finiteDiffs(ls, 2))
-    println(finiteDiffs(ls, 3))
-    println(finiteDiffs(ls, 4))
-
-    val a = BoxCox.lambdaSearch(ls.toList)
-    println(a)
-    val b = BoxCox.transform(ls.toList)
-    println(b)
 }
--- src/main/scala/bxcx/SARIMAModel.scala
+++ src/main/scala/bxcx/SARIMAModel.scala
@@ -2,12 +2,15 @@ package bxcx
 
 import breeze.linalg.{DenseMatrix, DenseVector, pinv}
 
+import scala.annotation.tailrec
+
 case class Season(period: Int, p: Int, d: Int, q: Int)
 
-class SARIMAModel(data: Vector[Double], p: Int, d: Int, q: Int, seasons: Seq[Season] = Nil) {
+class SARIMAModel(rawData: Vector[Double], p: Int, d: Int, q: Int, seasons: Seq[Season] = Nil) {
 
+    val data: Vector[Double] = rawData/*BoxCox.transform(rawData.toList).toVector*/
     val diffs: Vector[Double] = finiteDiffs(data, d)
-    val boxcoxed: Vector[Double] = BoxCox.transform(data.toList).toVector
+    val stationary: Vector[Double] = finiteDiffs(data, 1)
 
     def finiteDiffs(vector: Vector[Double], order: Int, lag: Int = 0): Vector[Double] = {
         if (order < 1) vector
@@ -18,34 +21,34 @@ class SARIMAModel(data: Vector[Double], p: Int, d: Int, q: Int, seasons: Seq[Sea
         }
     }
 
-    def avg(vector: Vector[Double]) = {
-        vector.sum/vector.size
-    }
-
     def vectorMultiplication(vector1: Vector[Double], vector2: Vector[Double]) =
         vector1 zip vector2 map { case (v1, v2) => v1 * v2 } sum
 
     def solve(a: DenseMatrix[Double], b: DenseVector[Double]): DenseVector[Double] =
         pinv(a) * b
 
-    def createVectors(dropFirst: Int, dropLast: Int) = {
-        data.indices.drop(dropFirst).dropRight(dropLast).map { idx =>
-            var v = (1.0 +: (1 to p).map(i => diffs(idx - i))) ++ (1 to q).map(j => boxcoxed(idx - j))
-            seasons.foreach { season =>
-                val sDiffs = finiteDiffs(data, season.d, season.period)
-                if (idx >= Seq(season.p, season.q, season.period).max) {
-                    v = v ++ (season.period until season.period + season.p).map(i => sDiffs(idx - i))
-                    v = v ++ (season.period until season.period + season.q).map(i => boxcoxed(idx - i - season.period))
-                } else v = v :+ 0 :+ 0
-            }
-            DenseVector(v: _ *)
+    def createVector(idx: Int, data: Vector[Double] = data) = {
+        val diffs: Vector[Double] = finiteDiffs(data, d)
+        val stationary: Vector[Double] = finiteDiffs(data, 1)
+        var v = (1.0 +: (1 to p).map(i => diffs(idx - i))) ++ (1 to q).map(j => stationary(idx - j))
+        seasons.foreach { season =>
+            val sDiffs = finiteDiffs(data, season.d, season.period)
+            if (idx > Seq(season.p, season.q, season.period).max) {
+                v = v ++ (season.period until season.period + season.p).map(i => sDiffs(idx - i - season.d))
+                v = v ++ (season.period until season.period + season.q).map(i => stationary(idx - i))
+            } else v = v :+ 0.0 :+ 0.0
         }
+        DenseVector(v: _ *)
+    }
+
+    def createVectors(dropFirst: Int, dropLast: Int) = {
+        data.indices.drop(dropFirst).dropRight(dropLast).map { idx => createVector(idx) }
     }
 
     val coefVectors = createVectors(Seq(p, q).max, d)
 
     val resultV = data.indices.drop(Seq(p, q).max).dropRight(d).map { idx =>
-        diffs(idx) - boxcoxed(idx)
+        diffs(idx) - stationary(idx)
     }
 
     val coefMatrix: DenseMatrix[Double] = DenseMatrix(coefVectors: _ *)
@@ -53,9 +56,15 @@ class SARIMAModel(data: Vector[Double], p: Int, d: Int, q: Int, seasons: Seq[Sea
 
     val abcCoenfs: DenseVector[Double] = solve(coefMatrix, resultVector)
 
-    def predict(n: Int) = {
-        ???
+    def predict(n: Int): Vector[Double] = {
+        val s = n + data.length
+        @tailrec
+        def go(data: Vector[Double]): Vector[Double] = {
+            if (data.length >= s) data else {
+                val newData = createVector(data.length - 1, data) * abcCoenfs
+                go(data ++ newData.data.toVector)
+            }
+        }
+        go(data)
     }
-
-
 }
